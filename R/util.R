0f5ab327762aa53f4599b006c9e3128ae5550d0c
Gianluca Bontempi <gianlucabontempi@iMac-Pro-de-Gianluca.local>
2019-04-08 10:41:47 +0200

    genTS

--- R/util.R
+++ R/util.R
@@ -697,70 +697,85 @@ genTS<-function(nn,NN,sd=0.5,num=1){
   Y=rnorm(nn)
   ep=0
   th0=rnorm(1)
+  fs<-sample(0:(n),4)
   if (num>0){
     for (i in 1:NN){
       N=length(Y)
       
       if (num==1){
-        fs<-sample(0:(n),2)
+        nfs=2
         e=rnorm(1)
         Y=c(Y,-0.4*(3-Y[N-fs[1]]^2)/(1+Y[N-fs[1]]^2)+0.6*(3-(Y[N-fs[2]]-0.5)^3)/(1+(Y[N-fs[2]]-0.5)^4)+sd*(e+th0*ep))
         ep=e
       }
       if (num==2){
-        fs<-sample(0:(n),2)
+        nfs=2
         e=rnorm(1)
         Y=c(Y,(0.4-2*exp(-50*Y[N-fs[1]]^2))*Y[N-fs[1]]+(0.5-0.5*exp(-50*Y[N-fs[2]]^2))*Y[N-fs[2]]+sd*(e+th0*ep))
         ep=e
       }
       if (num==3){
-        fs<-sample(0:(n),3)
+        nfs=3
         e=rnorm(1)
         Y=c(Y,1.5 *sin(pi/2*Y[N-fs[1]])-sin(pi/2*Y[N-fs[2]])+sin(pi/2*Y[N-fs[3]])+sd*(e+th0*ep))
         ep=e
       }
       if (num==4){
-        fs<-sample(0:(n),2)
+        nfs=2
         e=rnorm(1)
         Y=c(Y,2*exp(-0.1*Y[N-fs[1]]^2)*Y[N-fs[1]]-exp(-0.1*Y[N-fs[2]]^2)*Y[N-fs[2]]+sd*(e+th0*ep))
         ep=e
       }
       if (num==5){
-        fs<-sample(0:(n),1)
+        nfs=1
         Y=c(Y,-2*Y[N-fs[1]]*max(0,sign(-Y[N-fs[1]]))+0.4*Y[N-fs[1]]*max(0,sign(Y[N-fs[1]]))+sd*rnorm(1))
       }
       if (num==6){
-        fs<-sample(0:(n),2)
+        nfs=2
         Y=c(Y,0.8*log(1+3*Y[N-fs[1]]^2)-0.6*log(1+3*Y[N-fs[2]]^2)+sd*rnorm(1))
       }
       if (num==7){
-        fs<-sample(0:(n),2)
+        nfs=2
         Y=c(Y,1.5 *sin(pi/2*Y[N-fs[1]])-sin(pi/2*Y[N-fs[2]])+sd*rnorm(1))
       }
       if (num==8){
-        fs<-sample(0:(n),2)
+        nfs=2
         Y=c(Y,(0.5-1.1*exp(-50*Y[N-fs[1]]^2))*Y[N]+(0.3-0.5*exp(-50*Y[N-fs[2]]^2))*Y[N-fs[2]]+sd*rnorm(1))
       }
       if (num==9){
-        fs<-sample(0:(n),2)
+        nfs=2
         Y=c(Y,0.3*Y[N-fs[1]]+0.6*Y[N-fs[2]]+(0.1-0.9*Y[N-fs[1]]+0.8*Y[N-fs[2]])/(1+exp(-10*Y[N-fs[1]]))+sd*rnorm(1))
       }
       if (num==10){
-        fs<-sample(0:(n),1)
+        nfs=1
         Y=c(Y,sign(Y[N-fs[1]])+sd*rnorm(1))
       }
       if (num==11){
-        fs<-sample(0:(n),1)
+        nfs=1
         Y=c(Y,0.8*Y[N-fs[1]]-0.8*Y[N-fs[1]]/(1+exp(-10*Y[N-fs[1]]))+sd*rnorm(1))
       }
       
       if (num==12){
-        fs<-sample(0:(n),2)
+        nfs=2
         Y=c(Y,0.3*Y[N-fs[1]]+0.6*Y[N-fs[2]]+(0.1-0.9*Y[N-fs[1]]+0.8*Y[N-fs[2]])/(1+exp(-10*Y[N-fs[1]]))+sd*rnorm(1))
       }
       if (num==13){
-        fs<-sample(0:(n),1)
+        nfs=1
         Y=c(Y,0.246*Y[N-fs[1]]*(16-Y[N-fs[1]])+sd*rnorm(1))
+      }
+      if (num==14){
+        nfs=2
+        ## Henon map
+        Y=c(Y,1-1.4*Y[N-fs[1]]*Y[N-fs[1]] + 0.3*(Y[N-fs[2]])+sd*rnorm(1))
+      }
+      if (num==14){
+        nfs=1
+        ## TAR map
+        if (Y[N-fs[1]]<1){
+        Y=c(Y,-0.5*Y[N-fs[1]]+sd*rnorm(1))
+        } else {
+          Y=c(Y,0.4*Y[N-fs[1]]+sd*rnorm(1))
+        }
       }
       
     }
@@ -768,6 +783,7 @@ genTS<-function(nn,NN,sd=0.5,num=1){
   
   if (num<0){
     fs<- 1:(-num)
+    nfs=length(fs)
     ord=-num
     Cf=rnorm(ord)
     ma=rnorm(2)
@@ -777,6 +793,7 @@ genTS<-function(nn,NN,sd=0.5,num=1){
     Y<-c(arima.sim(n = NN, list(ar = Cf, ma = ma,sd = sd)))  
   }
   
+  fs=fs[1:nfs]
   Y=scale(Y[nn:length(Y)])
   if (any(is.nan(Y)))
     browser()
